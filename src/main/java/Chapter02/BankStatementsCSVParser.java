af1139db262ffa264c08f2f19cef3f3409073459
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-10-16 12:02:24 -0300

    Added BanKStatementsCSVParser class

---
+++ src/main/java/Chapter02/BankStatementsCSVParser.java
@@ -0,0 +1,28 @@
+package Chapter02;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankStatementsCSVParser {
+    private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    private BankTransaction parseFromCSV(String line) {
+        String[] columns = line.split(",");
+        LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+        double amount = Double.parseDouble(columns[1]);
+        String description = columns[2];
+
+        return new BankTransaction(date, amount, description);
+    }
+
+    public List<BankTransaction> parseLinesFromCSV(List<String> lines) {
+        List<BankTransaction> bankTransactions = new ArrayList<>();
+        for(String line: lines) {
+            bankTransactions.add(parseFromCSV(line));
+        }
+
+        return bankTransactions;
+    }
+}
