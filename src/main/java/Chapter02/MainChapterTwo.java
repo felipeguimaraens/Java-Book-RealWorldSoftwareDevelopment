d09fa543ace187e3e0e9d76dbf33dd0451c7489b
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-10-21 12:59:11 -0300

    Uncommented chapter02 main code

--- src/main/java/Chapter02/MainChapterTwo.java
+++ src/main/java/Chapter02/MainChapterTwo.java
@@ -2,9 +2,9 @@ package Chapter02;
 
 public class MainChapterTwo {
     public static void main(String[] args) throws Exception {
-        //BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
-        //BankStatementParser bankStatementParser = new BankStatementCSVParser();
+        BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
+        BankStatementParser bankStatementParser = new BankStatementCSVParser();
 
-        //bankStatementAnalyzer.analyze("bank-data-simple.csv", bankStatementParser);
+        bankStatementAnalyzer.analyze("bank-data-simple.csv", bankStatementParser);
     }
 }
