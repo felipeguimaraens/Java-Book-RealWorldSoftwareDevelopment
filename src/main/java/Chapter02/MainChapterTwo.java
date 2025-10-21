b49f0bb3247348889c65206ac60cff29d6d2f8fa
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-10-21 12:17:00 -0300

    Added main code for chapter 02

---
+++ src/main/java/Chapter02/MainChapterTwo.java
@@ -0,0 +1,10 @@
+package Chapter02;
+
+public class MainChapterTwo {
+    public static void main(String[] args) throws Exception {
+        //BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
+        //BankStatementParser bankStatementParser = new BankStatementCSVParser();
+
+        //bankStatementAnalyzer.analyze("bank-data-simple.csv", bankStatementParser);
+    }
+}
